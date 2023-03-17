3fd502e2fdaeb59fb8812e444c0c3208b5921939
Erickkirui <erickkirui653@gmail.com>
2023-03-17 13:28:18 +0300

    completed all tasks apart from overwriteBestCustomer

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavouriteCustomer = 'erick'
+function upperCaseCustomerName (){
+     
+customerName ='BOB'
+
+}
+//console.log(upperCaseCustomerName())
+function setBestCustomer(){
+
+       bestCustomer = "not bob";
+}
+
+//console.log(bestCustomer)
+
+function overwriteBestcustomer(){
+
+   bestCustomer= "mayb bob";
+
+}
+
+
+function changeLeastFavoriteCustomer(){
+   
+   leastFavouriteCustomer = 'mary'
+}
+
+
